17ed5fb057eb5016677e63e0e08425f72cbf4629
Lee Steven <l.stevennz@gmail.com>
2022-03-03 00:25:41 +1300

    updated matches.py file

--- smackbang/matches.py
+++ smackbang/matches.py
@@ -9,43 +9,79 @@ def get_matches(origin_one, origin_two, departure_date, return_date="", currency
     Returns df  index=['cityTo'],
     columns=[origin_one,origin_two,origin_one_price,origin_two_price,combined_price
     '''
+    # assigning url to be in used in get request
     url = "https://tequila-api.kiwi.com/v2/search"
+
+    # create airports_df from csv
     airports_df = pd.read_csv('../raw_data/airport_codes.csv')
+
+    # creating origins variable to used in get request
     origins = f'{origin_one},{origin_two}'
+
+    # creating fly_to variable to be used in get request
     destinations_ser = airports_df[airports_df['continent']==continent]['iata_code'].dropna()
     destinations_list = []
-
     for code in destinations_ser:
         destinations_list.append(code)
-
     if origin_one in destinations_list:
         destinations_list.remove(origin_one)
-
     if origin_two in destinations_list:
         destinations_list.remove(origin_two)
-
     fly_to = ','.join(destinations_list)
 
+    # creating headers
     headers = CaseInsensitiveDict()
     headers["accept"] = "application/json"
     headers["apikey"] = "Ul0VynVifRdwPUGTkFU_Btfro1vyewb7"
 
+    # creating query string to be used in get request
     query_string = {'fly_from':origins, 'fly_to':fly_to,
                 'date_from':departure_date, 'date_to':departure_date,'return_from':'','return_to':'',
                'adults':1, 'children':0,'selected_cabins':'M','adult_hold_bag':1,'adult_hand_bag':1,
                "curr":'USD', 'limit':1000}
 
-    resp = requests.get(url, headers=headers, params=query_string)
-    # print(origins)
-    # print(type(origins))
-    # print(departure_date)
-    # print(type(departure_date))
-    # print(fly_to)
-    # print(type(fly_to))
+    # creating response d in json format
+    d = requests.get(url, headers=headers, params=query_string).json()
 
-    print(resp.status_code)
+    # creating dictionary with desired keys
+    df = {'cityFrom':[], 'cityTo':[],'cityCodeFrom':[],'cityCodeTo':[], 'price':[], 'duration':[],
+          'total_stops':[],'distance':[],'local_departure':[],'local_arrival':[], "deep_link":[]
+    }
 
+    # importing response d into df
+    for i in range(0,len(d['data'])):
+        for k,v in d['data'][i].items():
+            if k == 'duration':
+                df[k].append(round(v['total']/3600,2))
+            elif k == 'id':
+                df['total_stops'].append(len(v.split('|'))-1)
+            else:
+                if k in df:
+                    df[k].append(v)
+
+    # creating dataframe df from dictionary df
+    df = pd.DataFrame(df)
+
+    # creating new df column "route"
+    df['route'] = df.cityCodeFrom + "-" + df.cityCodeTo
+
+    # outputting the cheapest flight for each route and sort values by the common destination
+    df = df.groupby('route').min().sort_values('cityTo')
+
+    # creating pivot table to compare flights from origin_one and orign_two to common destination
+    df = df.pivot(index='cityTo', columns='cityFrom', values=['price','duration','total_stops','distance','local_departure','local_arrival','deep_link'])
+
+    # creating new column for combined price
+    df['combined_price'] = df['price']['Sydney']+ df['price']['Tokyo']
+
+    # sorting df by combined price for the top common destinations by price
+    df = df.sort_values(by='combined_price')
+
+    # return top 5 common destinations by price
+
+
+    return df.head(20)
 
 if __name__ == "__main__":
 
-    get_matches('NRT','SYD','01/04/2022')
+    print(get_matches(origin_one='NRT',origin_two='SYD',departure_date='01/04/2022'))
