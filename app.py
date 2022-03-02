8c981884c42bc0ed4dbd13a00dca5917232805e8
Lee Steven <l.stevennz@gmail.com>
2022-03-02 17:33:24 +1300

    updated app.py for testing

--- app.py
+++ app.py
@@ -96,9 +96,9 @@ with row2_2:
             querystring = {"origin": origin_one, "page":page, "currency":currency , "depart_date": departure_date, "destination": destination}
 
             headers = {
-                'x-access-token': RAPID_API_TOKEN,
+                'x-access-token': "ccf49e56bc37cdcbea0545a0a08b7e08",
                 'x-rapidapi-host': "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
-                'x-rapidapi-key': RAPID_API_KEY
+                'x-rapidapi-key': "062d5d04d0msh9bf753a499a46f8p1d18edjsn469f78c5d3ac"
                     }
 
             response = requests.request("GET", url, headers=headers, params=querystring)
@@ -110,9 +110,9 @@ with row2_2:
             querystring = {"origin": origin_two, "page":page, "currency":currency , "depart_date":departure_date, "destination": destination}
 
             headers = {
-                'x-access-token': RAPID_API_TOKEN,
+                'x-access-token': "ccf49e56bc37cdcbea0545a0a08b7e08",
                 'x-rapidapi-host': "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
-                'x-rapidapi-key': RAPID_API_KEY
+                'x-rapidapi-key': "062d5d04d0msh9bf753a499a46f8p1d18edjsn469f78c5d3ac"
                       }
 
             response = requests.request("GET", url, headers=headers, params=querystring)
@@ -121,9 +121,9 @@ with row2_2:
 
         def get_city_location(cities):
             headers = {
-                'x-access-token': RAPID_API_TOKEN,
+                'x-access-token': "ccf49e56bc37cdcbea0545a0a08b7e08",
                 'x-rapidapi-host': "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com",
-                'x-rapidapi-key': RAPID_API_KEY
+                'x-rapidapi-key': "062d5d04d0msh9bf753a499a46f8p1d18edjsn469f78c5d3ac"
             }
 
             response = requests.request("GET", city_url, headers=headers).json()
